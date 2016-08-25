7d7cef0f96ebede56453445acbe0d43886c02af2
Alan R. Rogers <rogers@anthro.utah.edu>
2016-08-25 15:46:01 -0600

    Seems to work. No serious testing yet.

--- bedvcf.c
+++ bedvcf.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <assert.h>
 #include <string.h>
+#include <stdlib.h>
 
+// From documentation for bed format:
+//
 // chrom - The name of the chromosome (e.g. chr3, chrY, chr2_random)
 // or scaffold (e.g. scaffold10671).
 //
@@ -13,6 +16,18 @@
 // feature. For example, the first 100 bases of a chromosome are
 // defined as chromStart=0, chromEnd=100, and span the bases numbered
 // 0-99.
+//
+// From documentation for vcf format.
+//
+// POS - position:  The reference position, with the 1st base having
+// position 1.  Positions are sorted numerically, in increasing order,
+// within each reference sequence CHROM. It is permitted to have
+// multiple records with the same POS. Telomeres are indicated by
+// using positions 0 or N+1, where N is the length of the
+// corresponding chromosome or contig.  (Integer, Required)
+//
+// In this program, positions are base-0, so I subtract 1 from the
+// vcf-file position variable.
 
 #define CHRNAMESIZE 30
 
@@ -21,15 +36,19 @@ typedef struct Interval Interval;
 struct Interval {
 	char chr[CHRNAMESIZE];
 	long unsigned start; // 0-based index of start of interval
-	long undigned end;   // 1st position beyond end of interval
+	long unsigned end;   // 1st position beyond end of interval
 };
 
-int Interval_read(Interval *ivl, FILE *fp) {
+int Interval_read(Interval *self, FILE *fp);
+void Interval_print(const Interval *self, FILE *fp);
+
+int Interval_read(Interval *self, FILE *fp) {
 	char buff[300], *chr, *startToken, *endToken, *ptr;
+    int status;
 
 	if(NULL == fgets(buff, sizeof buff, fp))
 		return EOF;
-	if(NULL == strchr(buff, '\n')) {
+    if(NULL == strchr(buff, '\n') && !feof(fp)) {
 		fprintf(stderr,"%s:%s:%d: buffer overflow.\n",
 				__FILE__,__func__,__LINE__);
 		exit(EXIT_FAILURE);
@@ -39,12 +58,136 @@ int Interval_read(Interval *ivl, FILE *fp) {
 	startToken = strtok_r(buff, "\t", &ptr);
 	endToken = strtok_r(buff, "\t", &ptr);
 
-	if(NULL == endToken)
+	if(NULL == endToken) {
+        fprintf(stderr,"%s:%s:%d: Bad line in bed file.\n",
+                __FILE__,__func__,__LINE__);
+        exit(EXIT_FAILURE);
+    }
+
+    status = snprintf(self->chr, sizeof self->chr, "%s", chr);
+    if(status >= sizeof self->chr) {
+        fprintf(stderr,"%s:%s:%d: chromosome name too large. Max=%d; name=\"%s\"\n",
+                __FILE__,__func__,__LINE__, CHRNAMESIZE, chr);
+        exit(EXIT_FAILURE);
+    }
+
+    // In bed files, the first nucleotide is position 0. I'm using the
+    // same convention within this program, so no adjustment is needed
+    // here. 
+    self->start = strtoul(startToken, NULL, 10);
+    self->end = strtoul(endToken, NULL, 10);
+    return 0;
 }
 
+void Interval_print(const Interval *self, FILE *fp) {
+    fprintf(fp, "Ivl: %5s [%9lu, %9lu)\n", self->chr, self->start, self->end);
+}
+
+const char *usage = "usage: bedvcf file.bed < file.vcf";
 int main(int argc, char **argv) {
 
-	
+    if(argc != 2) {
+        fprintf(stderr, "%s\n", usage);
+        exit(EXIT_FAILURE);
+    }
+
+    FILE *bed = fopen(argv[1], "r");
+    if(bed==NULL) {
+        fprintf(stderr,"Can't open bed file \"%s\".\n", argv[1]);
+        fprintf(stderr, "%s\n", usage);
+        exit(EXIT_FAILURE);
+    }
+    
+    Interval ivl0, ivl;
+
+    if(EOF == Interval_read(&ivl, bed)) {
+        fprintf(stderr,"Can't read initial interval from bed file.\n");
+        exit(EXIT_FAILURE);
+    }
+    Interval_print(&ivl, stdout);
+
+    char buff[2048];
+    char chr[CHRNAMESIZE];
+    unsigned long pos;
+    while(1) {
+        if(NULL == fgets(buff, sizeof buff, stdin))
+            break;
+        if(NULL == strchr(buff, '\n') && !feof(stdin)) {
+            fprintf(stderr,"%s:%s:%d: buffer overflow.\n",
+                    __FILE__,__func__,__LINE__);
+            exit(EXIT_FAILURE);
+        }
+
+        // Header lines pass through unfiltered
+        if(buff[0] == '#') {
+            if(EOF == fputs(buff, stdout)) {
+                fprintf(stderr,"%s:%d: Error on output\n", __FILE__,__LINE__);
+                exit(EXIT_FAILURE);
+            }
+            continue;
+        }
+
+        // I'm tokenizing by hand, because strsep or strtok_r would
+        // modify buff, and I don't want to do that.
+
+        // tab1 and tab2 point to the first two tabs
+        char *tab1, *tab2=NULL;
+        char postok[CHRNAMESIZE];
+        tab1 = strchr(buff, '\t');
+        if(tab1 != NULL)
+            tab2 = strchr(tab1+1, '\t');
+        if(tab2==NULL) {
+            fprintf(stderr,"%s:%d: Bad line in vcf file:\n %s\n",
+                    __FILE__,__LINE__, buff);
+            exit(EXIT_FAILURE);
+        }
+
+        // copy first token into chr
+        int n = tab1 - buff;
+        strncpy(chr, buff, n);
+        chr[n] = '\0';
+
+        // copy 2nd token into postok
+        tab1 += 1;
+        n = tab2 - tab1;
+        strncpy(postok, tab1, n);
+        postok[n] = '\0';
+
+        strtoul(postok, NULL, 10);
+        if(pos==0) // in telomere
+            continue;
+        --pos;  // make vcf positions consistent with bed positions
+
+        if(pos < ivl.start)
+            // We're not yet at the start of interval, so read vcf.
+            continue;
+
+        while(pos >= ivl.end) {
+            // We're beyond end of interval so read bed file.
+            ivl0 = ivl;
+            if(EOF == Interval_read(&ivl, bed)) {
+                goto done;
+            }
+            Interval_print(&ivl, stdout);
+            if(strcmp(ivl0.chr, ivl.chr) == 0 && ivl0.start > ivl.start) {
+                fprintf(stderr,"%s:%d: bed file isn't sorted.\n",
+                        __FILE__, __LINE__);
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        // New interval doesn't include current position: read vcf.
+        if(pos < ivl.start)
+            continue;
+
+        // In the interval: print a line of output.
+        assert(pos >= ivl.start && pos < ivl.end);
+        if(EOF == fputs(buff, stdout)) {
+            fprintf(stderr,"%s:%d: Error on output\n", __FILE__,__LINE__);
+            exit(EXIT_FAILURE);
+        }
+    }
+ done:
 	return 0;
 }
 
