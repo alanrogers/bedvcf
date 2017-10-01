d88291adbcc016cfcaaec6bf64a04498f6644b8f
Alan R. Rogers <rogers@anthro.utah.edu>
2017-10-01 07:13:27 -0600

    Input buffer size 4096

--- bedvcf.c
+++ bedvcf.c
@@ -162,7 +162,7 @@ int main(int argc, char **argv) {
     }
     //    Interval_print(&ivl, stdout);
 
-    char        buff[16384];
+    char        buff[4096];
     char        chr[CHRNAMESIZE] = {'\0'}, chr0[CHRNAMESIZE];
     unsigned long pos=0, pos0;
     int         chrdiff = -1;
